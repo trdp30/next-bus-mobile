c028e20fac55a6f4a97ea15c8b12a50e93aeeda2
trdp30 <trdp30@gmail.com>
2024-11-16 17:40:03 +0530

    bg task stop functionality added

--- index.js
+++ index.js
@@ -5,19 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {backgroundTask} from './src/utils/backgroundTasks';
 
 AppRegistry.registerComponent(appName, () => App);
 
-const backgroundTask = async taskData => {
-  return new Promise((resolve, reject) => {
-    console.log('taskData', taskData);
-    setInterval(() => {
-      console.log('task', new Date().toLocaleTimeString());
-      resolve();
-    }, 5000);
-  });
-};
-
-AppRegistry.registerHeadlessTask('BackgroundTask', () =>
-  backgroundTask.bind(this),
-);
+AppRegistry.registerHeadlessTask('BackgroundTask', () => backgroundTask);
--- src/utils/backgroundTasks.js
+++ src/utils/backgroundTasks.js
@@ -2,9 +2,10 @@ import {NativeModules} from 'react-native';
 
 const {BackgroundTaskModule} = NativeModules;
 
+let timer = false;
+
 export const startBackgroundService = async () => {
   try {
-    console.log('startBackgroundService', BackgroundTaskModule);
     BackgroundTaskModule.startBackgroundTask('bar');
   } catch (e) {
     console.error(e);
@@ -13,4 +14,17 @@ export const startBackgroundService = async () => {
 
 export const stopBackgroundService = async () => {
   // BackgroundTaskModule.stopBackgroundTask();
+  clearInterval(timer);
+};
+
+export const backgroundTask = async taskData => {
+  return new Promise((resolve, reject) => {
+    console.log('taskData', taskData);
+    timer = setInterval(() => {
+      console.log('task', new Date().toLocaleTimeString());
+      if (!timer) {
+        resolve();
+      }
+    }, 5000);
+  });
 };
