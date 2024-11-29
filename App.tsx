70951c2fdd0f868d51698faa38dec18605cec804
trdp30 <trdp30@gmail.com>
2024-11-29 16:45:29 +0530

    gluestack installed

--- App.tsx
+++ App.tsx
@@ -5,8 +5,10 @@
  * @format
  */
 
+import {GluestackUIProvider} from '@/src/components/ui/gluestack-ui-provider';
 import React from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
+import './global.css';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import BottomTabNavigation from './src/Navigation/BottomTabNavigation';
@@ -19,13 +21,13 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <>
+    <GluestackUIProvider mode={isDarkMode ? 'dark' : 'light'}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <BottomTabNavigation />
-    </>
+    </GluestackUIProvider>
   );
 }
 
